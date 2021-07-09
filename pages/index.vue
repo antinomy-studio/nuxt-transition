793972b8f747e1ecf9b6c6a215ec25a0ef52b225
baptistebriel <baptiste@sa-studio.fr>
2021-07-09 16:16:06 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+const getPage = (vm, route) => {
+  vm.$children.forEach((el) => console.log(el._name, el))
+  return vm.$children.find(child => child.$route.name === route)
+}
+
+export default {
+  transition: {
+    mode: '',
+    enter: function (el, done) {
+      const page = getPage(this, 'index')
+      console.log('enter', this._uid, page._uid)
+      done()
+    }
+  },
+  mounted() {
+    console.log('mounted', this._uid)
+  }
+}
+</script>
